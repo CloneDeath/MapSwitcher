3b973ca603e135d277fc97545a280c52096113e4
Nicholas Rodine <halfofastaple@gmail.com>
2014-03-15 02:29:50 -0400

    Updated GUI, moved around controls. Made Apply button big.

--- MapSwitcher/MapSwitcher/MainForm.Designer.cs
+++ MapSwitcher/MapSwitcher/MainForm.Designer.cs
@@ -41,9 +41,13 @@
 			this.label4 = new System.Windows.Forms.Label();
 			this.cbVersions = new System.Windows.Forms.ComboBox();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.cbEnemyTeamAI = new System.Windows.Forms.ComboBox();
 			this.btnRandomHeroes = new System.Windows.Forms.Button();
 			this.label11 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
 			this.label12 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.cbAllyTeamAI = new System.Windows.Forms.ComboBox();
 			this.cbHeroDropdown10 = new System.Windows.Forms.ComboBox();
 			this.cbHeroDropdown9 = new System.Windows.Forms.ComboBox();
 			this.label13 = new System.Windows.Forms.Label();
@@ -65,22 +69,16 @@
 			this.groupBox3 = new System.Windows.Forms.GroupBox();
 			this.btnLoadMap = new System.Windows.Forms.Button();
 			this.btnStartGame = new System.Windows.Forms.Button();
-			this.groupBox4 = new System.Windows.Forms.GroupBox();
-			this.cbEnemyTeamAI = new System.Windows.Forms.ComboBox();
-			this.label6 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.cbAllyTeamAI = new System.Windows.Forms.ComboBox();
 			this.groupBox1.SuspendLayout();
 			this.groupBox2.SuspendLayout();
 			this.groupBox3.SuspendLayout();
-			this.groupBox4.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// btnApply
 			// 
-			this.btnApply.Location = new System.Drawing.Point(344, 452);
+			this.btnApply.Location = new System.Drawing.Point(341, 346);
 			this.btnApply.Name = "btnApply";
-			this.btnApply.Size = new System.Drawing.Size(94, 23);
+			this.btnApply.Size = new System.Drawing.Size(94, 50);
 			this.btnApply.TabIndex = 0;
 			this.btnApply.Text = "Apply";
 			this.btnApply.UseVisualStyleBackColor = true;
@@ -99,7 +97,7 @@
 			// 
 			this.cbHeroDropdown1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.cbHeroDropdown1.FormattingEnabled = true;
-			this.cbHeroDropdown1.Location = new System.Drawing.Point(62, 47);
+			this.cbHeroDropdown1.Location = new System.Drawing.Point(62, 57);
 			this.cbHeroDropdown1.Name = "cbHeroDropdown1";
 			this.cbHeroDropdown1.Size = new System.Drawing.Size(146, 21);
 			this.cbHeroDropdown1.TabIndex = 2;
@@ -130,11 +128,11 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(11, 51);
+			this.label2.Location = new System.Drawing.Point(11, 61);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(42, 13);
+			this.label2.Size = new System.Drawing.Size(50, 13);
 			this.label2.TabIndex = 5;
-			this.label2.Text = "Hero 1:";
+			this.label2.Text = "My Hero:";
 			// 
 			// label3
 			// 
@@ -176,13 +174,17 @@
 			// 
 			// groupBox1
 			// 
+			this.groupBox1.Controls.Add(this.cbEnemyTeamAI);
 			this.groupBox1.Controls.Add(this.btnRandomHeroes);
 			this.groupBox1.Controls.Add(this.label11);
 			this.groupBox1.Controls.Add(this.label12);
+			this.groupBox1.Controls.Add(this.label6);
 			this.groupBox1.Controls.Add(this.cbHeroDropdown10);
 			this.groupBox1.Controls.Add(this.cbHeroDropdown9);
 			this.groupBox1.Controls.Add(this.label13);
+			this.groupBox1.Controls.Add(this.label5);
 			this.groupBox1.Controls.Add(this.cbHeroDropdown8);
+			this.groupBox1.Controls.Add(this.cbAllyTeamAI);
 			this.groupBox1.Controls.Add(this.label14);
 			this.groupBox1.Controls.Add(this.cbHeroDropdown7);
 			this.groupBox1.Controls.Add(this.label15);
@@ -202,14 +204,23 @@
 			this.groupBox1.Controls.Add(this.cbHeroDropdown1);
 			this.groupBox1.Location = new System.Drawing.Point(12, 73);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(426, 219);
+			this.groupBox1.Size = new System.Drawing.Size(426, 262);
 			this.groupBox1.TabIndex = 10;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Map and Heroes";
 			// 
+			// cbEnemyTeamAI
+			// 
+			this.cbEnemyTeamAI.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbEnemyTeamAI.FormattingEnabled = true;
+			this.cbEnemyTeamAI.Location = new System.Drawing.Point(265, 229);
+			this.cbEnemyTeamAI.Name = "cbEnemyTeamAI";
+			this.cbEnemyTeamAI.Size = new System.Drawing.Size(146, 21);
+			this.cbEnemyTeamAI.TabIndex = 3;
+			// 
 			// btnRandomHeroes
 			// 
-			this.btnRandomHeroes.Location = new System.Drawing.Point(265, 182);
+			this.btnRandomHeroes.Location = new System.Drawing.Point(265, 192);
 			this.btnRandomHeroes.Name = "btnRandomHeroes";
 			this.btnRandomHeroes.Size = new System.Drawing.Size(146, 23);
 			this.btnRandomHeroes.TabIndex = 25;
@@ -220,26 +231,53 @@
 			// label11
 			// 
 			this.label11.AutoSize = true;
-			this.label11.Location = new System.Drawing.Point(214, 158);
+			this.label11.Location = new System.Drawing.Point(214, 168);
 			this.label11.Name = "label11";
 			this.label11.Size = new System.Drawing.Size(48, 13);
 			this.label11.TabIndex = 24;
 			this.label11.Text = "Hero 10:";
 			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(211, 232);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(55, 13);
+			this.label6.TabIndex = 2;
+			this.label6.Text = "Enemy AI:";
+			// 
 			// label12
 			// 
 			this.label12.AutoSize = true;
-			this.label12.Location = new System.Drawing.Point(214, 131);
+			this.label12.Location = new System.Drawing.Point(214, 141);
 			this.label12.Name = "label12";
 			this.label12.Size = new System.Drawing.Size(42, 13);
 			this.label12.TabIndex = 23;
 			this.label12.Text = "Hero 9:";
 			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(10, 232);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(39, 13);
+			this.label5.TabIndex = 1;
+			this.label5.Text = "Ally AI:";
+			// 
+			// cbAllyTeamAI
+			// 
+			this.cbAllyTeamAI.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbAllyTeamAI.FormattingEnabled = true;
+			this.cbAllyTeamAI.Location = new System.Drawing.Point(62, 229);
+			this.cbAllyTeamAI.Name = "cbAllyTeamAI";
+			this.cbAllyTeamAI.Size = new System.Drawing.Size(146, 21);
+			this.cbAllyTeamAI.TabIndex = 0;
+			// 
 			// cbHeroDropdown10
 			// 
 			this.cbHeroDropdown10.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.cbHeroDropdown10.FormattingEnabled = true;
-			this.cbHeroDropdown10.Location = new System.Drawing.Point(265, 155);
+			this.cbHeroDropdown10.Location = new System.Drawing.Point(265, 165);
 			this.cbHeroDropdown10.Name = "cbHeroDropdown10";
 			this.cbHeroDropdown10.Size = new System.Drawing.Size(146, 21);
 			this.cbHeroDropdown10.TabIndex = 22;
@@ -248,7 +286,7 @@
 			// 
 			this.cbHeroDropdown9.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.cbHeroDropdown9.FormattingEnabled = true;
-			this.cbHeroDropdown9.Location = new System.Drawing.Point(265, 128);
+			this.cbHeroDropdown9.Location = new System.Drawing.Point(265, 138);
 			this.cbHeroDropdown9.Name = "cbHeroDropdown9";
 			this.cbHeroDropdown9.Size = new System.Drawing.Size(146, 21);
 			this.cbHeroDropdown9.TabIndex = 21;
@@ -256,7 +294,7 @@
 			// label13
 			// 
 			this.label13.AutoSize = true;
-			this.label13.Location = new System.Drawing.Point(214, 104);
+			this.label13.Location = new System.Drawing.Point(214, 114);
 			this.label13.Name = "label13";
 			this.label13.Size = new System.Drawing.Size(42, 13);
 			this.label13.TabIndex = 20;
@@ -266,7 +304,7 @@
 			// 
 			this.cbHeroDropdown8.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.cbHeroDropdown8.FormattingEnabled = true;
-			this.cbHeroDropdown8.Location = new System.Drawing.Point(265, 101);
+			this.cbHeroDropdown8.Location = new System.Drawing.Point(265, 111);
 			this.cbHeroDropdown8.Name = "cbHeroDropdown8";
 			this.cbHeroDropdown8.Size = new System.Drawing.Size(146, 21);
 			this.cbHeroDropdown8.TabIndex = 19;
@@ -274,7 +312,7 @@
 			// label14
 			// 
 			this.label14.AutoSize = true;
-			this.label14.Location = new System.Drawing.Point(214, 77);
+			this.label14.Location = new System.Drawing.Point(214, 87);
 			this.label14.Name = "label14";
 			this.label14.Size = new System.Drawing.Size(42, 13);
 			this.label14.TabIndex = 18;
@@ -284,7 +322,7 @@
 			// 
 			this.cbHeroDropdown7.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.cbHeroDropdown7.FormattingEnabled = true;
-			this.cbHeroDropdown7.Location = new System.Drawing.Point(265, 74);
+			this.cbHeroDropdown7.Location = new System.Drawing.Point(265, 84);
 			this.cbHeroDropdown7.Name = "cbHeroDropdown7";
 			this.cbHeroDropdown7.Size = new System.Drawing.Size(146, 21);
 			this.cbHeroDropdown7.TabIndex = 17;
@@ -292,7 +330,7 @@
 			// label15
 			// 
 			this.label15.AutoSize = true;
-			this.label15.Location = new System.Drawing.Point(214, 51);
+			this.label15.Location = new System.Drawing.Point(214, 61);
 			this.label15.Name = "label15";
 			this.label15.Size = new System.Drawing.Size(42, 13);
 			this.label15.TabIndex = 16;
@@ -302,7 +340,7 @@
 			// 
 			this.cbHeroDropdown6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.cbHeroDropdown6.FormattingEnabled = true;
-			this.cbHeroDropdown6.Location = new System.Drawing.Point(265, 47);
+			this.cbHeroDropdown6.Location = new System.Drawing.Point(265, 57);
 			this.cbHeroDropdown6.Name = "cbHeroDropdown6";
 			this.cbHeroDropdown6.Size = new System.Drawing.Size(146, 21);
 			this.cbHeroDropdown6.TabIndex = 15;
@@ -310,7 +348,7 @@
 			// label10
 			// 
 			this.label10.AutoSize = true;
-			this.label10.Location = new System.Drawing.Point(11, 158);
+			this.label10.Location = new System.Drawing.Point(11, 168);
 			this.label10.Name = "label10";
 			this.label10.Size = new System.Drawing.Size(42, 13);
 			this.label10.TabIndex = 14;
@@ -319,7 +357,7 @@
 			// label9
 			// 
 			this.label9.AutoSize = true;
-			this.label9.Location = new System.Drawing.Point(11, 131);
+			this.label9.Location = new System.Drawing.Point(11, 141);
 			this.label9.Name = "label9";
 			this.label9.Size = new System.Drawing.Size(42, 13);
 			this.label9.TabIndex = 13;
@@ -329,7 +367,7 @@
 			// 
 			this.cbHeroDropdown5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.cbHeroDropdown5.FormattingEnabled = true;
-			this.cbHeroDropdown5.Location = new System.Drawing.Point(62, 155);
+			this.cbHeroDropdown5.Location = new System.Drawing.Point(62, 165);
 			this.cbHeroDropdown5.Name = "cbHeroDropdown5";
 			this.cbHeroDropdown5.Size = new System.Drawing.Size(146, 21);
 			this.cbHeroDropdown5.TabIndex = 12;
@@ -338,7 +376,7 @@
 			// 
 			this.cbHeroDropdown4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.cbHeroDropdown4.FormattingEnabled = true;
-			this.cbHeroDropdown4.Location = new System.Drawing.Point(62, 128);
+			this.cbHeroDropdown4.Location = new System.Drawing.Point(62, 138);
 			this.cbHeroDropdown4.Name = "cbHeroDropdown4";
 			this.cbHeroDropdown4.Size = new System.Drawing.Size(146, 21);
 			this.cbHeroDropdown4.TabIndex = 11;
@@ -346,7 +384,7 @@
 			// label8
 			// 
 			this.label8.AutoSize = true;
-			this.label8.Location = new System.Drawing.Point(11, 104);
+			this.label8.Location = new System.Drawing.Point(11, 114);
 			this.label8.Name = "label8";
 			this.label8.Size = new System.Drawing.Size(42, 13);
 			this.label8.TabIndex = 10;
@@ -356,7 +394,7 @@
 			// 
 			this.cbHeroDropdown3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.cbHeroDropdown3.FormattingEnabled = true;
-			this.cbHeroDropdown3.Location = new System.Drawing.Point(62, 101);
+			this.cbHeroDropdown3.Location = new System.Drawing.Point(62, 111);
 			this.cbHeroDropdown3.Name = "cbHeroDropdown3";
 			this.cbHeroDropdown3.Size = new System.Drawing.Size(146, 21);
 			this.cbHeroDropdown3.TabIndex = 9;
@@ -364,7 +402,7 @@
 			// label7
 			// 
 			this.label7.AutoSize = true;
-			this.label7.Location = new System.Drawing.Point(11, 77);
+			this.label7.Location = new System.Drawing.Point(11, 87);
 			this.label7.Name = "label7";
 			this.label7.Size = new System.Drawing.Size(42, 13);
 			this.label7.TabIndex = 8;
@@ -374,7 +412,7 @@
 			// 
 			this.cbHeroDropdown2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.cbHeroDropdown2.FormattingEnabled = true;
-			this.cbHeroDropdown2.Location = new System.Drawing.Point(62, 74);
+			this.cbHeroDropdown2.Location = new System.Drawing.Point(62, 84);
 			this.cbHeroDropdown2.Name = "cbHeroDropdown2";
 			this.cbHeroDropdown2.Size = new System.Drawing.Size(146, 21);
 			this.cbHeroDropdown2.TabIndex = 7;
@@ -384,7 +422,7 @@
 			this.cbUnlockTalents.AutoSize = true;
 			this.cbUnlockTalents.Checked = true;
 			this.cbUnlockTalents.CheckState = System.Windows.Forms.CheckState.Checked;
-			this.cbUnlockTalents.Location = new System.Drawing.Point(14, 186);
+			this.cbUnlockTalents.Location = new System.Drawing.Point(14, 196);
 			this.cbUnlockTalents.Name = "cbUnlockTalents";
 			this.cbUnlockTalents.Size = new System.Drawing.Size(98, 17);
 			this.cbUnlockTalents.TabIndex = 6;
@@ -396,9 +434,9 @@
 			this.groupBox2.Controls.Add(this.label3);
 			this.groupBox2.Controls.Add(this.tbName);
 			this.groupBox2.Controls.Add(this.btnChangeName);
-			this.groupBox2.Location = new System.Drawing.Point(12, 298);
+			this.groupBox2.Location = new System.Drawing.Point(12, 341);
 			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(426, 55);
+			this.groupBox2.Size = new System.Drawing.Size(323, 55);
 			this.groupBox2.TabIndex = 11;
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "Name";
@@ -436,68 +474,18 @@
 			this.btnStartGame.UseVisualStyleBackColor = true;
 			this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
 			// 
-			// groupBox4
-			// 
-			this.groupBox4.Controls.Add(this.cbEnemyTeamAI);
-			this.groupBox4.Controls.Add(this.label6);
-			this.groupBox4.Controls.Add(this.label5);
-			this.groupBox4.Controls.Add(this.cbAllyTeamAI);
-			this.groupBox4.Location = new System.Drawing.Point(12, 359);
-			this.groupBox4.Name = "groupBox4";
-			this.groupBox4.Size = new System.Drawing.Size(426, 87);
-			this.groupBox4.TabIndex = 13;
-			this.groupBox4.TabStop = false;
-			this.groupBox4.Text = "AI Difficulty";
-			// 
-			// cbEnemyTeamAI
-			// 
-			this.cbEnemyTeamAI.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.cbEnemyTeamAI.FormattingEnabled = true;
-			this.cbEnemyTeamAI.Location = new System.Drawing.Point(87, 47);
-			this.cbEnemyTeamAI.Name = "cbEnemyTeamAI";
-			this.cbEnemyTeamAI.Size = new System.Drawing.Size(160, 21);
-			this.cbEnemyTeamAI.TabIndex = 3;
-			// 
-			// label6
-			// 
-			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(11, 50);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(68, 13);
-			this.label6.TabIndex = 2;
-			this.label6.Text = "Enemy team:";
-			// 
-			// label5
-			// 
-			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(11, 23);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(58, 13);
-			this.label5.TabIndex = 1;
-			this.label5.Text = "Your team:";
-			// 
-			// cbAllyTeamAI
-			// 
-			this.cbAllyTeamAI.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.cbAllyTeamAI.FormattingEnabled = true;
-			this.cbAllyTeamAI.Location = new System.Drawing.Point(87, 20);
-			this.cbAllyTeamAI.Name = "cbAllyTeamAI";
-			this.cbAllyTeamAI.Size = new System.Drawing.Size(160, 21);
-			this.cbAllyTeamAI.TabIndex = 0;
-			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(455, 482);
-			this.Controls.Add(this.groupBox4);
+			this.ClientSize = new System.Drawing.Size(451, 403);
 			this.Controls.Add(this.groupBox3);
 			this.Controls.Add(this.groupBox2);
 			this.Controls.Add(this.groupBox1);
 			this.Controls.Add(this.btnApply);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.Name = "MainForm";
-			this.Text = "HotS Patcher";
+			this.Text = "0";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
 			this.Load += new System.EventHandler(this.MainForm_Load);
 			this.groupBox1.ResumeLayout(false);
@@ -506,8 +494,6 @@
 			this.groupBox2.PerformLayout();
 			this.groupBox3.ResumeLayout(false);
 			this.groupBox3.PerformLayout();
-			this.groupBox4.ResumeLayout(false);
-			this.groupBox4.PerformLayout();
 			this.ResumeLayout(false);
 
         }
@@ -527,8 +513,7 @@
         private System.Windows.Forms.ComboBox cbVersions;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.ComboBox cbEnemyTeamAI;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
